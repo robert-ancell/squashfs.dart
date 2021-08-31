7c6bb35bb9821d4a52447a16a108f7652f697629
Robert Ancell <robert.ancell@gmail.com>
2021-08-31 23:55:31 +1200

    Start decoding inode table

--- lib/src/squashfs_file.dart
+++ lib/src/squashfs_file.dart
@@ -1,9 +1,66 @@
 import 'dart:io';
 import 'dart:typed_data';
 
+enum SquashFsCompression { none, gzip, lzma, lzo, xz, lz4, zstd }
+
+class SquashfsInode {
+  final int uidIdx;
+  final int gidIdx;
+  final int modifiedTime;
+  final int inodeNumber;
+
+  SquashfsInode(
+      {required this.uidIdx,
+      required this.gidIdx,
+      required this.modifiedTime,
+      required this.inodeNumber});
+}
+
+class SquashfsBasicDirectoryInode extends SquashfsInode {
+  SquashfsBasicDirectoryInode(
+      {required int uidIdx,
+      required int gidIdx,
+      required int modifiedTime,
+      required int inodeNumber})
+      : super(
+            uidIdx: uidIdx,
+            gidIdx: gidIdx,
+            modifiedTime: modifiedTime,
+            inodeNumber: inodeNumber);
+}
+
+class SquashfsBasicFileInode extends SquashfsInode {
+  SquashfsBasicFileInode(
+      {required int uidIdx,
+      required int gidIdx,
+      required int modifiedTime,
+      required int inodeNumber})
+      : super(
+            uidIdx: uidIdx,
+            gidIdx: gidIdx,
+            modifiedTime: modifiedTime,
+            inodeNumber: inodeNumber);
+}
+
+class SquashfsBasicSymlinkInode extends SquashfsInode {
+  SquashfsBasicSymlinkInode(
+      {required int uidIdx,
+      required int gidIdx,
+      required int modifiedTime,
+      required int inodeNumber})
+      : super(
+            uidIdx: uidIdx,
+            gidIdx: gidIdx,
+            modifiedTime: modifiedTime,
+            inodeNumber: inodeNumber);
+}
+
 class SquashfsFile {
   final File _file;
 
+  late final Endian _endian;
+  late final int _blockSize;
+  late final SquashFsCompression _compression;
   late final int _flags;
   late final int _idTableStart;
   late final int _xattrIdTableStart;
@@ -11,6 +68,8 @@ class SquashfsFile {
   late final int _directoryTableStart;
   late final int _fragmentTableStart;
   late final int _exportTableStart;
+
+  late final List<SquashfsInode> _inodes;
 
   SquashfsFile(String filename) : _file = File(filename);
 
@@ -22,38 +81,132 @@ class SquashfsFile {
       throw 'Insufficient data for squashfs superblock';
     }
     var buffer = ByteData.sublistView(superblock);
-    var endian = Endian.little;
-    var magic = buffer.getUint32(0, endian);
+    _endian = Endian.little;
+    var magic = buffer.getUint32(0, _endian);
     if (magic != 0x73717368) {
-      endian = Endian.big;
-      magic = buffer.getUint32(0, endian);
+      _endian = Endian.big;
+      magic = buffer.getUint32(0, _endian);
       if (magic != 0x73717368) {
         throw 'Invalid magic';
       }
     }
-    var inodeCount = buffer.getUint32(4, endian);
-    var modificationTime = buffer.getUint32(8, endian);
-    var blockSize = buffer.getUint32(12, endian);
-    var fragmentEntryCount = buffer.getUint32(16, endian);
-    var compressionId = buffer.getUint16(20, endian);
-    var blockLog = buffer.getUint16(22, endian);
-    _flags = buffer.getUint16(24, endian);
-    var idCount = buffer.getUint16(26, endian);
-    var versionMajor = buffer.getUint16(28, endian);
-    var versionMinor = buffer.getUint16(30, endian);
-    var rootInodeRef = buffer.getUint64(32, endian);
-    var bytesUsed = buffer.getUint64(40, endian);
-    _idTableStart = buffer.getUint64(48, endian);
-    _xattrIdTableStart = buffer.getUint64(56, endian);
-    _inodeTableStart = buffer.getUint64(64, endian);
-    _directoryTableStart = buffer.getUint64(72, endian);
-    _fragmentTableStart = buffer.getUint64(80, endian);
-    _exportTableStart = buffer.getUint64(88, endian);
+    var inodeCount = buffer.getUint32(4, _endian);
+    var modificationTime = buffer.getUint32(8, _endian);
+    _blockSize = buffer.getUint32(12, _endian);
+    var fragmentEntryCount = buffer.getUint32(16, _endian);
+    var compressionId = buffer.getUint16(20, _endian);
+    if (compressionId >= SquashFsCompression.values.length) {
+      throw 'Unknown compression ID $compressionId';
+    }
+    _compression = SquashFsCompression.values[compressionId];
+    var blockLog = buffer.getUint16(22, _endian);
+    _flags = buffer.getUint16(24, _endian);
+    var idCount = buffer.getUint16(26, _endian);
+    var versionMajor = buffer.getUint16(28, _endian);
+    var versionMinor = buffer.getUint16(30, _endian);
+    var rootInodeRef = buffer.getUint64(32, _endian);
+    var bytesUsed = buffer.getUint64(40, _endian);
+    _idTableStart = buffer.getUint64(48, _endian);
+    _xattrIdTableStart = buffer.getUint64(56, _endian);
+    _inodeTableStart = buffer.getUint64(64, _endian);
+    _directoryTableStart = buffer.getUint64(72, _endian);
+    _fragmentTableStart = buffer.getUint64(80, _endian);
+    _exportTableStart = buffer.getUint64(88, _endian);
 
     print('version = $versionMajor.$versionMinor');
-    print('compression = $compressionId');
+    print('compression = $_compression');
     print('flags = ${_flags.toRadixString(2)}');
 
+    _inodes = await _readInodeTable(file, inodeCount);
+    print(_inodes);
+
     file.close();
+  }
+
+  Future<Uint8List> _readMetadata(
+      RandomAccessFile file, int start, int end) async {
+    var builder = BytesBuilder();
+    var offset = start;
+    while (offset < end) {
+      await file.setPosition(offset);
+      var blockLength =
+          ByteData.sublistView(await file.read(2)).getUint16(0, _endian);
+      var data = await file.read(blockLength & 0x7fff);
+      if (blockLength & 0x8000 == 0) {
+        builder.add(_decompress(data));
+      } else {
+        builder.add(data);
+      }
+      offset += 2 + blockLength & 0x7fff;
+    }
+    return builder.takeBytes();
+  }
+
+  List<int> _decompress(Uint8List data) {
+    switch (_compression) {
+      case SquashFsCompression.gzip:
+        return gzip.decode(data);
+      default:
+        throw 'Unsupported compression format $_compression';
+    }
+  }
+
+  Future<List<SquashfsInode>> _readInodeTable(
+      RandomAccessFile file, int inodeCount) async {
+    var inodes = <SquashfsInode>[];
+
+    var data =
+        await _readMetadata(file, _inodeTableStart, _directoryTableStart);
+    var buffer = ByteData.sublistView(data);
+
+    var offset = 0;
+    for (var i = 0; i < inodeCount; i++) {
+      var inodeType = buffer.getUint16(offset + 0, _endian);
+      var permissions = buffer.getUint16(offset + 2, _endian);
+      var uidIdx = buffer.getUint16(offset + 4, _endian);
+      var gidIdx = buffer.getUint16(offset + 6, _endian);
+      var modifiedTime = buffer.getUint32(offset + 8, _endian);
+      var inodeNumber = buffer.getUint32(offset + 12, _endian);
+      offset += 16;
+
+      switch (inodeType) {
+        case 1: // Basic Directory
+          var dirBlockStart = buffer.getUint32(offset + 0, _endian);
+          var hardLinkCount = buffer.getUint32(offset + 4, _endian);
+          var fileSize = buffer.getUint16(offset + 8, _endian);
+          var blockOffset = buffer.getUint16(offset + 10, _endian);
+          var parentInodeNumber = buffer.getUint32(offset + 12, _endian);
+          offset += 16;
+          inodes.add(SquashfsBasicDirectoryInode(
+              uidIdx: uidIdx,
+              gidIdx: gidIdx,
+              modifiedTime: modifiedTime,
+              inodeNumber: inodeNumber));
+          break;
+        case 2: // Basic File
+          var blocksStart = buffer.getUint32(offset + 0, _endian);
+          var fragmentBlockIndex = buffer.getUint32(offset + 4, _endian);
+          var blockOffset = buffer.getUint32(offset + 8, _endian);
+          var fileSize = buffer.getUint32(offset + 12, _endian);
+          offset += 16;
+          var nFullBlocks = fileSize ~/ _blockSize;
+          var blockSizes = <int>[];
+          for (var j = 0; j < nFullBlocks; j++) {
+            var blockSize = buffer.getUint32(offset, _endian);
+            blockSizes.add(blockSize);
+            offset += 4;
+          }
+          inodes.add(SquashfsBasicFileInode(
+              uidIdx: uidIdx,
+              gidIdx: gidIdx,
+              modifiedTime: modifiedTime,
+              inodeNumber: inodeNumber));
+          break;
+        default:
+          throw 'Unknown inode type $inodeType';
+      }
+    }
+
+    return inodes;
   }
 }
